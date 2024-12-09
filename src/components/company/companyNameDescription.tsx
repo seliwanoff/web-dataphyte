08f57d9aa7844037484e0be4d59142657f7052f1
seliwanoff <olagunjuselim2019@gmail.com>
2024-12-09 20:30:40 +0100

    refactor

--- src/components/company/companyNameDescription.tsx
+++ src/components/company/companyNameDescription.tsx
@@ -10,7 +10,7 @@ const ComapnyNameDescription: React.FC<CompanyNameDescriptionProps> = ({
   meta,
 }) => {
   return (
-    <div className="w-full xl:px-[110px] px-[24px] py-[32px]">
+    <div className="w-full xl:px-[110px] px-[24px] py-[32px] bg-[#7F55DA0A]">
       <div className="w-full  max-w-[1750px] mx-auto">
         <div className="w-fit mx-auto flex flex-col gap-12 max-w-[897px]">
           <h3 className="font-Poppins xl:text-[42px]  text-[32px] font-semibold leading-[72px] text-center text-gradient p-0 m-0">
